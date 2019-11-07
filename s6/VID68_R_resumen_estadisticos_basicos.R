f09d9e1bdd2e405dcb78e7f76725b695ceecf099
Andres <andresmontealegregiraldo@mail.com>
2019-11-06 21:06:13 -0500

    VID 6

---
+++ s6/VID68_R_resumen_estadisticos_basicos.R
@@ -0,0 +1,4 @@
+#install.packages(c('modeest','raster','moments'))
+library(modeest) #moda
+library(raster) #quantiles y Coefi variacion
+library(moments) #Momentos, Coeficiente de asimetría curtosis
